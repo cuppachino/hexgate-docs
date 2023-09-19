0998fa324aa4311f9aabea08df625e71dd5ca152
cuppachino <cuppachino.dev@gmail.com>
2023-09-19 17:57:49 -0500

    fix 404 error

--- scripts/make-404.js
+++ scripts/make-404.js
@@ -3,9 +3,15 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const source_html = resolve(__dirname, "../index.html");
-const target_html = resolve(__dirname, "../404.html");
+const source_html = resolve(__dirname, "../dist/index.html");
+const target_html = resolve(__dirname, "../dist/404.html");
 
 import { copyFile } from "fs/promises";
 
-await copyFile(source_html, target_html);
+copyFile(source_html, target_html)
+  .then(() => {
+    console.log("copied dist/index.html to dist/404.html");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
